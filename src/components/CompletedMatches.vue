92e5bb5498591a67fe980f7d27afc9830deaba72
jhangstein <joseph.hangstein@stud.fhcampus-wien.ac.at>
2022-01-20 15:28:47 +0100

    Display matches page fully functional.

--- src/components/CompletedMatches.vue
+++ src/components/CompletedMatches.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="vue-template">
     <div id="screenIGuess" class="promo inner-block">
-      <table>
+      <table v-if="movies">
         <tr>
           <th>Movie</th>
           <th>Description</th>
           <th>Rating</th>
         </tr>
-        <tr v-for="movie in 10" :key="movie" >
-            <td><img width=250 src="https://www.joblo.com/wp-content/uploads/2022/01/blacklight-neeson.jpg" alt="fuckoff"><br><p><b>Ur cooler Film oida</b></p></td>
-            <td>Travis Block is a government operative coming to terms with his shadowy past. When he discovers a plot targeting U.S. citizens, Block finds himself in the crosshairs of the FBI director he once helped protect.</td>
-            <td>Rating 8/10</td>
+        <tr v-for="(movie, index) in this.movies.data" :key="movie" >
+            <td><img width=250 :src="movies.data[index].poster_path"><br><p><b>{{movies.data[index].original_title}}</b></p></td>
+            <td>{{movies.data[index].overview}}</td>
+            <td>Rating: {{movies.data[index].vote_average}}/10</td>
         </tr>
       </table>
     </div>
@@ -37,13 +37,24 @@ export default {
     const loggedInUser = this.user._id;
     const otherUser = this.$route.params.otherUser
 
-    this.movies = await axios.get('/getCommonMovies', {
-      params: {
-        requestingFriend: loggedInUser,
-        requestedFriend: otherUser
+    try {
+      this.movies = await axios.get('/getCommonMovies', {
+        params: {
+          requestingFriend: loggedInUser,
+          requestedFriend: otherUser
+        }
+      })
+      console.log(this.movies);
+
+      for (let i = 0; i<this.movies.data.length; i++){
+        let help = this.movies.data[i].poster_path;
+        this.movies.data[i].poster_path = "https://image.tmdb.org/t/p/w500" + help;
       }
-    })
-    console.log(this.movies)
+      console.log(this.movies);
+
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
 </script>
